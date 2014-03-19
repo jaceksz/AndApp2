862ab91fb51739e73eee87a366661220da9683af
Jacek Szlaczka <jacek.szlaczka@gmail.com>
2014-03-19 23:29:27 +0100

    Android app 1.0

---
+++ src/com/example/helloworld/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.helloworld;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        Button myButton = (Button) findViewById(R.id.wyszukaj);
+        myButton.setOnClickListener(new OnClickListener());
+        {
+        	public void onClick(view v)
+        	{
+        		
+        	}
+        }
+        
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    
+}
